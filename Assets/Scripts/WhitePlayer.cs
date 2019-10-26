633d5a253db1777e333a51b88ac29c2291dda8cd
Your Name <you@example.com>
2019-10-26 15:21:26 +0200

    Player jumping added.

--- Assets/Scripts/WhitePlayer.cs
+++ Assets/Scripts/WhitePlayer.cs
@@ -4,35 +4,98 @@ using UnityEngine;
 
 public class WhitePlayer : MonoBehaviour
 {
-		public string horizontalInputName;
+	public string horizontalInputName;
+	public string verticalInputName;
 
-		public float movementSpeed = 5.0f;
-		public float jumpSpeed = 5.0f;
+	public float movementSpeed = 5.0f;
 
-		private Rigidbody2D rb;
+	private Rigidbody2D rb;
 
-		public KeyCode jumpKey;
+	private bool isJumping = false;
+	public float jumpSpeed = 5.0f;
+	public float maxJumpTime = 0.2f;
+	private float jumpButtonPressTime = 0.0f;
+	
+	private float rayCastLength = 0.005f;
 
-		private bool isJumping = false;
-		
-    // Start is called before the first frame update
-    void Awake()
-    {
-			rb = GetComponent<Rigidbody2D> ();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        PlayerMovement();
-    }
-
-		private void PlayerMovement()
-		{
-			float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
-
-			Vector2 vect = rb.velocity;
+	private float width;
+	private float height;
  
-			rb.velocity = new Vector2 (horizInput * movementSpeed, vect.y);
+	//////////////////// Unity main functions. ////////////////////
+
+  void Awake()
+  {
+		width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+		height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
+
+		rb = GetComponent<Rigidbody2D>();	
+  }
+
+  void FixedUpdate()
+  {
+		PlayerMovement();
+		PlayerJump();
+  }
+		
+	//////////////////// My functions. ////////////////////
+
+	private void PlayerMovement()
+	{
+		float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+
+		Vector2 vect = rb.velocity;
+ 
+		rb.velocity = new Vector2 (horizInput * movementSpeed, vect.y);
+	}
+
+	private void PlayerJump()
+	{
+		float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+		// Debug.Log(vertInput);
+
+		if (IsOnGround () && isJumping == false)
+		{
+			if (vertInput > 0f)
+			{
+				isJumping = true;
+			}
 		}
+
+		if (jumpButtonPressTime > maxJumpTime)
+		{
+			vertInput = 0f;
+		}
+ 
+		if (isJumping && (jumpButtonPressTime <= maxJumpTime))
+		{
+			rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
+		}
+ 
+		if (vertInput >= 1f)
+		{
+			jumpButtonPressTime += Time.deltaTime;
+		}
+		else
+		{
+			isJumping = false;
+			jumpButtonPressTime = 0f;
+		}
+	}
+
+	public bool IsOnGround()
+	{
+		bool groundCheckDown = Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y - height), -Vector2.up, rayCastLength);
+ 
+		bool groundCheckRight = Physics2D.Raycast (new Vector2 (transform.position.x + (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLength);
+ 
+		bool groundCheckLeft = Physics2D.Raycast (new Vector2 (transform.position.x - (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLength);
+ 
+		if (groundCheckDown || groundCheckRight || groundCheckLeft)
+		{
+			return true;
+		}
+ 
+		return false; 
+	}
 }

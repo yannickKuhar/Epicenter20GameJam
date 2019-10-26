c5732cf77bba8cdfa589620fac660caec60326cd
Brin <brin@zvan.cc>
2019-10-26 18:11:00 +0200

    Advancment in button press

--- Assets/Scripts/BasicButtonPress.cs
+++ Assets/Scripts/BasicButtonPress.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class BasicButtonPress : MonoBehaviour
 {
     public bool activated = false;
+    public bool playerOnTop = false;
     public GameObject Player;
     public GameObject execution;
+    public Animator animator;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Update()
     {
-        if (activated == false && collision.collider.name == Player.name)
+        /*if (transform.position.y < -0.05)
         {
-            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            this.GetComponent<Rigidbody2D>().mass = 1;
-            activated = true;
-        }
+            animator.SetBool("ButtonDown", false);
+        }             */
+    }
 
-        if (collision.collider.name == "ButtonBottom")
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (activated == false && col.name == Player.name)
         {
-            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            try
-            {
-                execution.GetComponent<ExecuteButton>().ExecuteCommand();
-            }
-            catch
-            {
-                execution.GetComponentInChildren<ExecuteButton>().ExecuteCommand();
-            }                
+            /*this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            this.GetComponent<Rigidbody2D>().mass = 1;
+            activated = true; */
+            animator.SetTrigger("ButtonPressed");
+           
         }
     }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (activated == false && col.name == Player.name)
+        {
+            /*this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            this.GetComponent<Rigidbody2D>().mass = 1;
+            activated = true; */
+            animator.SetTrigger("ButtonUnpressed");
+
+        }
+    }
+    /* private void OnCollisionExit2D(Collision2D collision)
+     {
+         if (collision.collider.tag == "Player" )
+         {
+             animator.SetBool("PlayerWentOff", true);
+         }
+     }    */
 }

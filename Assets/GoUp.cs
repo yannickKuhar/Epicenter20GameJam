476b9389d7b429dc2f7211a2e096781a3bef04ea
Brin <brin@zvan.cc>
2019-10-26 22:51:55 +0200

    Unity animator???

---
+++ Assets/GoUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoUp : MonoBehaviour
+{
+    [Header("Fetcher")]
+    [SerializeField] private BasicButtonPress triggerButton;
+    [SerializeField] private Animator animator;
+    
+    [Header("Status")]
+    [SerializeField] private bool up;
+    void Start()
+    {
+        up = false;
+    }
+
+    void Update()
+    {
+        if (triggerButton.GetComponent<BasicButtonPress>().pressed == true  && !up)
+        {
+            Debug.Log("We're in boys");
+            up = true;
+            animator.SetTrigger("BoxGoUp");
+        }
+    }
+}

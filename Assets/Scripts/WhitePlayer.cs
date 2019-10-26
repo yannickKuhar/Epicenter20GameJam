842d20fcb0630c62838479d299a9d46671519f4d
Your Name <you@example.com>
2019-10-26 13:48:41 +0200

    Player movement added.

--- Assets/Scripts/WhitePlayer.cs
+++ Assets/Scripts/WhitePlayer.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WhitePlayer : MonoBehaviour
-{		
+{
+		public string horizontalInputName;
 
-		public float speed = 10.0f;
+		public float movementSpeed = 5.0f;
+		public float jumpSpeed = 5.0f;
+
+		private Rigidbody2D rb;
+
+		public KeyCode jumpKey;
+
+		private bool isJumping = false;
 		
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+			rb = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerMovement();
     }
+
+		private void PlayerMovement()
+		{
+			float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+
+			Vector2 vect = rb.velocity;
+ 
+			rb.velocity = new Vector2 (horizInput * movementSpeed, vect.y);
+		}
 }

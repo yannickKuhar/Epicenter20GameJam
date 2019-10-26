6fb95183f01a82d696931d526c01a227c53f7295
Krebsys <anze.krebs-setina@hotmail.com>
2019-10-26 17:26:42 +0100

    Button fix

--- Assets/Scripts/BasicButtonPress.cs
+++ Assets/Scripts/BasicButtonPress.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class BasicButtonPress : MonoBehaviour
 {
     public bool activated = false;
-    public GameObject Player;
+    public GameObject[] Player;
     public GameObject execution;
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (activated == false && collision.collider.name == Player.name)
+    {        
+        if (activated == false && collision.collider.name == Player[0].name)
+        {
+            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            this.GetComponent<Rigidbody2D>().mass = 1;
+            activated = true;
+        }
+        
+        else if (activated == false && Player.Length == 2)
         {
             this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             this.GetComponent<Rigidbody2D>().mass = 1;
@@ -27,7 +34,11 @@ public class BasicButtonPress : MonoBehaviour
             catch
             {
                 execution.GetComponentInChildren<ExecuteButton>().ExecuteCommand();
-            }                
+            }
+            finally
+            {
+                Debug.Log("Button Press Error.");
+            }
         }
     }
 }

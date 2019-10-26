2082e430ab057c56b6d18fd0543dd40823fd58a1
Brin <brin@zvan.cc>
2019-10-26 23:43:59 +0200

    Started creating win checkers.

---
+++ Assets/WinChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinChecker : MonoBehaviour
+{
+    [SerializeField] private WinLoader loader;
+    [SerializeField] private string playerColor = "Black";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" + playerColor)
+        {
+            Debug.Log("Commence win trigger");
+
+            if (playerColor == "Black")
+            { 
+                Debug.Log("player je crn, dodaj crni check");
+                loader.GetComponent<WinLoader>().blackCheck = true;
+            }
+            if (playerColor == "White")
+            {
+                loader.GetComponent<WinLoader>().whiteCheck = true;
+            }
+        }
+    }
+}
---
+++ Assets/WinLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoader : MonoBehaviour
+{
+    [SerializeField] public bool blackCheck, whiteCheck;
+
+    private void Start()
+    {
+        blackCheck = false;
+        whiteCheck = false;
+    }
+    void Update()
+    {
+        if (blackCheck && whiteCheck)
+        {
+            Debug.Log("Load next lvl");
+            //SceneManager.LoadScene(Level2)
+        }
+    }
+}

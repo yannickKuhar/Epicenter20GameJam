a319098aaa84c650fcc5858fbb4fd54443544dd6
Krebsys <anze.krebs-setina@hotmail.com>
2019-10-26 16:53:23 +0100

    Button and execution

---
+++ Assets/Scripts/BasicButtonPress.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicButtonPress : MonoBehaviour
+{
+    public bool activated = false;
+    public GameObject Player;
+    public GameObject execution;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (activated == false && collision.collider.name == Player.name)
+        {
+            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            this.GetComponent<Rigidbody2D>().mass = 1;
+            activated = true;
+        }
+
+        if (collision.collider.name == "ButtonBottom")
+        {
+            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            try
+            {
+                execution.GetComponent<ExecuteButton>().ExecuteCommand();
+            }
+            catch
+            {
+                execution.GetComponentInChildren<ExecuteButton>().ExecuteCommand();
+            }                
+        }
+    }
+}

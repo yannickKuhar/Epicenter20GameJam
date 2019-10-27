a86631c67a834874f86872e1e16dceadde2aa208
Your Name <you@example.com>
2019-10-27 14:04:58 +0100

    Respawn sys ad player die added.

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+	public float resetTime = 3.0f;
+	
+	public string whiteStr = "White player died! Level will reset.";
+	public string blackStr = "Black player died! Level will reset.";
+
+	public Text white;
+	public Text black;
+
+  void Update()
+  {
+		if (GameObject.Find("PlayerWhite") == null)
+		{
+			white.text = whiteStr;
+			StartCoroutine(Reset(white));
+		}
+		
+		if (GameObject.Find("PlayerBlack") == null)
+		{
+			black.text = blackStr;
+			StartCoroutine(Reset(black));
+		}
+  }
+
+	private void SceneReset()
+	{
+  }
+	
+	IEnumerator Reset(Text text)
+  {
+    yield return new WaitForSeconds(resetTime);
+		Debug.Log("Scene reset");
+		text.text = "";
+		Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
+  }
+}

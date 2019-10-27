e339ff5abd6c7e7765661e36767712a380761bae
Your Name <you@example.com>
2019-10-27 11:07:31 +0100

    Jump fix and SaveSys added (to be tested)

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelData
+{
+	int levelsComplete;
+
+	public LevelData(int levelsComplete)
+	{
+		levelsComplete = levelsComplete;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -136,16 +136,6 @@ public class PlayerController : MonoBehaviour
 		return facingRight;
 	}
 	
-	//////////////////// Collisions. ////////////////////
-	void OnCollisionEnter2D(Collision2D collision)
-  {
-		if (collision.gameObject.tag == "Wall")
-		{
-			Debug.Log("coll");
-			rb.velocity = new Vector2(-2, 0);
-		}
-	}
-
   /////////////////// Win conditions //////////////
 
 }

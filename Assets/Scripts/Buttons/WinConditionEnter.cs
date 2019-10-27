dd3e0f911b68d70bbbaba35b3d10f7887d18a004
Brin <brin@zvan.cc>
2019-10-27 14:49:32 +0100

    level 2 in vec animacij.

--- Assets/Scripts/Buttons/WinConditionEnter.cs
+++ Assets/Scripts/Buttons/WinConditionEnter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -8,6 +9,7 @@ public class WinConditionEnter : MonoBehaviour
 {
     [Header("Fethcer")]
     public WinLoader loader;
+    [SerializeField] private Text blackInfo, whiteInfo;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,18 +18,33 @@ public class WinConditionEnter : MonoBehaviour
         {
             Debug.Log("Player is off the button.");
             loader.gameObject.GetComponent<WinLoader>().blackCheck = true;
+            blackInfo.text = "Black player on check point. Waiting for black.";
             Debug.Log("Black player checked");
         }
         if (collision.gameObject.tag == "PlayerWhite")
         {
             Debug.Log("Player is off the button.");
             loader.gameObject.GetComponent<WinLoader>().whiteCheck = true;
+            whiteInfo.text = "Black player on check point. Waiting for White.";
             Debug.Log("White player checked");
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-
+        if (collision.gameObject.tag == "PlayerBlack")
+        {
+            Debug.Log("Player is off the button.");
+            loader.gameObject.GetComponent<WinLoader>().blackCheck = false;
+            blackInfo.text = "";
+            Debug.Log("Black player unchecked");
+        }
+        if (collision.gameObject.tag == "PlayerWhite")
+        {
+            Debug.Log("Player is off the button.");
+            loader.gameObject.GetComponent<WinLoader>().whiteCheck = false;
+            whiteInfo.text = "";
+            Debug.Log("White player unchecked");
+        }
     }
 
 

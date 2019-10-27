134b761b0cccd77be45c73721d7780377ccf91e5
Krebsys <anze.krebs-setina@hotmail.com>
2019-10-27 02:04:40 +0100

    Main Menu and BoxPrefab

---
+++ Assets/Scripts/Main Menu/SettingsSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsSound : MonoBehaviour
+{
+    public AudioMixer volumeAudio;
+    
+    void UpdateSoundSettings()
+    {
+        volumeAudio.SetFloat("Master", this.gameObject.GetComponent<Scrollbar>().value);
+    }
+}
